7b443fad08fe8777bcfca91a5f402f9b089a89ec
d.huy237 <d.huy723@gmail.com>
2020-12-05 21:13:59 +0700

    modified employee

--- employee.php
+++ employee.php
@@ -6,41 +6,40 @@ $connection = array("Database"=>"BankingDatabase", "UID"=>"bankowner", "PWD"=>"T
 $conn = sqlsrv_connect($server_name, $connection);
 
 if ($conn) {
-    echo "Connection established: " . $server_name;
+    // For testing
+    // echo "Connection established: " . $server_name;
 }
 else {
     echo "Connection could not be established";
     die(print_r(sqlsrv_errors(), true));
 }
 
-// Simple select query
-// $sql = "SELECT B_name, E_code, E_FirstName, E_LastName, E_DateOfBirth FROM Employee";
-//     $result = sqlsrv_query($conn, $sql);
-
-//     echo "<table>
-//     <tr>
-//         <th>B_name</th>
-//         <th>E_code</th>
-//         <th>E_FirstName</th>
-//         <th>E_LastName</th>
-//         <th>E_DateOfBirth</th>
-//     </tr>";
-
-//     while($row = sqlsrv_fetch_array($result))
-//     {
-//         echo "<tr>";
-//         echo "<td>" . $row['B_name'] . "</td>";
-//         echo "<td>" . $row['E_code'] . "</td>";
-//         echo "<td>" . $row['E_FirstName'] . "</td>";
-//         echo "<td>" . $row['E_LastName'] . "</td>";
-//         echo "<td>" . $row['E_DateOfBirth']->format('Y-m-d') . "</td>";
-//         echo "</tr>";
-//     }
-//     echo "</table>";
-
-//     sqlsrv_close($conn);
-
-
-
+if (!empty($_GET['employee_id'])){
+    // 'id' input from the user
+    $id = $_GET['employee_id'];
+    // Search id like $_GET['employee_id']
+    $sql = "SELECT B_name, E_code, E_FirstName, E_LastName, E_DateOfBirth FROM Employee WHERE E_code LIKE '" . $id . "%';";
+    $query = sqlsrv_query($conn, $sql);
+    
+    echo "<table style=\"margin-left: auto; margin-right: auto;\">
+    <tr>
+        <th>B_name</th>
+        <th>E_code</th>
+        <th>E_FirstName</th>
+        <th>E_LastName</th>
+        <th>E_DateOfBirth</th>
+    </tr>";
+    while($row = sqlsrv_fetch_array($query))
+    {
+        echo "<tr>";
+        echo "<td>" . $row['B_name'] . "</td>";
+        echo "<td>" . $row['E_code'] . "</td>";
+        echo "<td>" . $row['E_FirstName'] . "</td>";
+        echo "<td>" . $row['E_LastName'] . "</td>";
+        echo "<td>" . $row['E_DateOfBirth']->format('Y-m-d') . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+}
 sqlsrv_close($conn);
 ?>
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function showSearch() {
     var xhttp;
     var str = document.getElementById("search_text").value;
+    console.log(str);
 
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("table").innerHTML = this.responseText;
+            document.getElementById("employee_id").innerHTML = this.responseText;
         }
     };
-    xhttp.open("GET", "employee.php?table="+str, true);
+    xhttp.open("GET", "employee.php?employee_id="+str, true);
     xhttp.send();
 }
